6ea626a1591398ead2132ee1d5d98491ad086af5
chantifa <chantifa@windowslive.com>
2025-02-18 21:57:00 +0100

    deleted task.py

--- task.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-from src.database import engine
-
-# Example for training data:
-df = pd.read_csv('training_data.csv')
-df.to_sql('training_data', con=engine, if_exists='replace', index=False)
